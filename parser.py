568a74a8118484d8a14f65858b6c1bc048031666
ctf <ctf@u-Dell-System-XPS-L322X.(none)>
2012-12-01 15:47:39 +0800

    initial import

---
+++ parser.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from HTMLParser import HTMLParser
+import urllib, urllib2
+
+#keys_utf8 and kid_keys should be synced.
+keys_utf8 = ["姓名","性別","現在年齡","失蹤年齡","失蹤日期","特徵","失蹤地區","失蹤地點","失蹤原因"]
+kid_keys = ["name","sex","age","lost_age","lost_date","character","area","spot","reason","id","photo_url"]
+kid = {}
+kids = []
+
+
+class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_302(self, req, fp, code, msg, headers):
+        print "Cookie Manip Right Here"
+        return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+
+    http_error_301 = http_error_303 = http_error_307 = http_error_302
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+	current_key=None
+	tags_to_data=None
+
+	def handle_starttag(self, tag, attrs):
+		""
+			
+	def handle_endtag(self, tag):
+		""
+			
+	def handle_data(self, data):
+		data = unicode(data,'Big5','ignore').encode('utf-8','ignore')
+
+		if self.current_key == None:
+			for key in keys_utf8:
+				if key in data:
+					self.current_key = key
+					self.tags_to_data = 3
+		else:
+			self.tags_to_data = self.tags_to_data-1
+
+		if self.tags_to_data == 0:
+			i = keys_utf8.index(self.current_key)
+			i = kid_keys[i]
+			kid[i] = data.strip()
+			self.tags_to_data = None
+			self.current_key = None
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+
+http_handler = urllib2.HTTPHandler(debuglevel=0)
+redirect_handler = urllib2.HTTPRedirectHandler()
+
+handlers = [http_handler,redirect_handler]
+
+opener = urllib2.build_opener(http_handler,redirect_handler)
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+response = opener.open("http://www.example.com/200.html")
+
+baseurl = "http://www.missingkids.org.tw/chinese/focus.php"
+parameter = "?mode=show&temp=0&id="
+
+for i in range(30):
+	id = i+90
+	kid["id"] = id
+	response = opener.open(baseurl+parameter+str(id))
+	
+	print response.code
+
+	html=response.read()
+
+	parser.feed(html)
+
+	for k,v in kid.iteritems():
+		print str(k)+"="+str(v)
+
+	kids.append(kid)
+
