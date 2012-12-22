cebf0f980b12818585775875d483929dafec3248
timchen86 <tim.chen.86@gmail.com>
2012-12-22 22:19:38 +0800

    RAW_CSV for parsed data, SORTED_CSV for sorted.

--- cnf/settings.py
+++ cnf/settings.py
@@ -5,6 +5,7 @@
 #
 #     http://doc.scrapy.org/topics/settings.html
 #
+import os
 
 BOT_NAME = 'cnf'
 
@@ -13,6 +14,12 @@ NEWSPIDER_MODULE = 'cnf.spiders'
 
 ID_START = 1
 ID_END = 200
+
+RAW_CSV = os.path.expanduser('~/%s_raw.csv' % BOT_NAME) 
+SORTED_CSV = os.path.expanduser('~/%s.csv' % BOT_NAME)
+
+FEED_URI = RAW_CSV
+FEED_FORMAT = "CSV"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cnf (+http://www.yourdomain.com)'
