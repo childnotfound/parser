e165a5105a17ba697c591df3c7e35620c0050b04
timchen86 <tim.chen.86@gmail.com>
2012-12-22 17:55:23 +0800

    set default debug level to INFO

--- uploader.py
+++ uploader.py
@@ -11,7 +11,7 @@ import json
 import pprint
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger( __name__ )
 
 import httplib2
